da8caa644438c1896b1392bb7bc738056a2ee396
ken.shirakawa <shirakawa.ken.38w@st.kyoto-u.ac.jp>
2024-08-29 18:00:00 +0900

    add analysis script

---
+++ analysis/data-analysis/umap_transform_nsd_text_features_parameter_sweep.py
@@ -0,0 +1,81 @@
+# %%
+import os
+from itertools import product
+import pickle
+import numpy as np
+import pandas as pd
+import umap as up
+
+import bdpy
+from bdpy.dataform import Features
+
+from umap_utils import extract_features, concatenate_features, norm_feat
+
+# %%
+def main():
+    #network setting
+    network = 'pytorch/brain_diffuser_versatile_diffusion/'
+    feature_name = 'text_encoder'
+    # %%
+    # save dir setting
+    save_base_dir = './results'
+    assert os.path.isdir(save_base_dir)
+   
+
+    # %%
+    # feature path setting
+    feat_dir_dict= {
+        # NSD dataset [Allen et al, 2021]
+        'nsd-train': f'/home/nu/data/contents_shared/NSD-stimuli/derivatives/features/{network}',
+        'nsd-test':  f'/home/nu/data/contents_shared/NSD-stimuli/derivatives/features/{network}',
+    }
+
+    # stimulus id path setting
+    stim_id_dict = {
+        # NSD dataset [Allen et al, 2021]
+        'nsd-train': f'/home/nu/data/contents_shared/NSD-stimuli/derivatives/nsd-analysis/nsd_stimulus_category_id_sub01_unique.csv',
+        'nsd-test':  f'/home/nu/data/contents_shared/NSD-stimuli/derivatives/nsd-analysis/nsd_stimulus_category_id_sub01_shared1000.csv',
+    }
+
+    # Extract features using the new function
+    features_dict = extract_features(feat_dir_dict, stim_id_dict, feature_name)
+
+    # %%
+    feats = concatenate_features(features_dict)
+
+
+    min_dist_list = [0.0, 0.01, 0.05, 0.1, 0.5, 1.0]
+    n_neighbors_list = [5, 15, 30, 50, 100]
+    for min_dist, n_neighbors in product(min_dist_list, n_neighbors_list):
+        save_dir = os.path.join(save_base_dir, 'param_sweep_umap', network, feature_name)
+        os.makedirs(save_dir, exist_ok=True)
+        print(save_dir)
+        # %%
+        # perform umap
+        # perform umap for clustering by default parameters
+        umap = up.UMAP(metric='cosine',
+                            n_neighbors=n_neighbors,
+                            min_dist=min_dist,
+                            n_components=2,
+                            random_state=42)
+        # %%
+        # normalize features
+        feats_norm, m, s = norm_feat(feats)
+        # perform umap
+        embeddings = umap.fit_transform(feats_norm)
+        np.save(f'{save_dir}/nsd_embedding_norm_md_{min_dist}_nn_{n_neighbors}.npy', embeddings)
+        print(f'Finish analysis saved at {save_dir}')
+
+
+        if not os.path.isfile(f'{save_dir}/nsd_mean_md_{min_dist}_nn_{n_neighbors}.npy'):
+            np.save(f'{save_dir}/nsd_mean_md_{min_dist}_nn_{n_neighbors}.npy', m)
+        if not os.path.isfile(f'{save_dir}/nsd_norm_md_{min_dist}_nn_{n_neighbors}.npy'):
+            np.save(f'{save_dir}/nsd_norm_md_{min_dist}_nn_{n_neighbors}.npy', s)
+
+        # save umap 
+        umap_name = f'nsd_umap_md_{min_dist}_nn_{n_neighbors}.pkl'
+        if not os.path.isfile(os.path.join(save_dir, umap_name)):
+            pickle.dump(umap, open(os.path.join(save_dir, umap_name), 'wb'))
+
+if __name__ == "__main__":
+    main()
